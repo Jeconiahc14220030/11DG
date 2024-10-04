186125d751fda3e1bd4874d2ac3cf1aa561070ea
Christian Adi <c14220002@john.petra.ac.id>
2024-10-04 22:53:30 +0700

    login page

---
+++ Website_user/src/routes/login/+page.svelte
@@ -0,0 +1,25 @@
+<div class="h-screen w-screen flex flex-col" style="background-image: url('../src/lib/image/salib.jpg'); background-size: cover;">
+    <div class="flex justify-center items-center flex-1">
+        <img src="../src/lib/image/logo_white.png" alt="">
+    </div>
+    <div class="bg-base flex flex-col flex-1 opacity-50">
+        <div class="flex justify-center">
+            <p class="text-white text-5xl font-bold p-4">Log In</p>
+        </div>
+        <div class="flex justify-center">
+            <p class="text-white w-1/5">Enter your Username to Login for this app</p>
+        </div>
+        <div class="flex justify-center mt-4">
+            <input type="text" placeholder="Username" class="w-1/5 placeholder-black">
+        </div>
+        <div class="flex justify-center mt-4">
+            <input type="password" placeholder="Password" class="w-1/5 placeholder-black">
+        </div>
+        <div class="flex justify-center mt-6">
+            <button class="bg-[#F9C067] text-white px-6 py-2 rounded hover:bg-[#F8B048] font-bold">Login</button>
+        </div>
+        <div class="flex justify-center mt-2">
+            <a href="#" class="text-[#F9C067] underline">Forgot Password</a>
+        </div>
+    </div>
+</div>
--- Website_user/src/routes/products/[productId]/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    import { page } from '$app/stores';
-    const productId = $page.params.productId;
-</script>
-
-<h1>Details About Product {productId}</h1>
