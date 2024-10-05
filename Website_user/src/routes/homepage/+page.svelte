80da685224ef511dfbc7a9f3bbc58b62ebbfbf82
Christian Adi <c14220002@john.petra.ac.id>
2024-10-05 20:45:27 +0700

    create forgot_password, homepage

---
+++ Website_user/src/routes/homepage/+page.svelte
@@ -0,0 +1,62 @@
+<div class="h-screen w-screen flex flex-col bg-[#F4F4F4]">
+    <header class="flex items-center justify-between p-8 bg-[#F9C067] mb-16 h-16">
+        <div class="flex items-center">
+            <!-- Konten untuk header pertama -->
+        </div>
+    </header> 
+    <div class="flex flex-col ml-6 mr-6">
+        <div class="flex justify-end -mt-16"> <!-- Menggunakan margin negatif untuk menggeser gambar ke atas -->
+            <img src="../src/lib/image/logo.png" alt="logo" class="w-16 h-16">
+        </div>
+
+        <!-- Visi Misi -->
+        <div class="bg-[#FEFEFE] flex flex-col">
+            <div class="m-4">
+                <div class="text-[#F0A242] font-bold ml-4 text-2xl">
+                    <h1>Visi Misi</h1>
+                </div>
+                <div class="ml-4">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Pesan Ketua -->
+        <div class="bg-[#FEFEFE] flex flex-col mt-6">
+            <div class="m-4">
+                <div class="text-[#F0A242] font-bold ml-4 text-2xl">
+                    <h1>Pesan Ketua</h1>
+                </div>
+                <div class="ml-4">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Renungan -->
+        <div class="relative bg-cover bg-center w-full h-64 rounded-lg shadow-lg flex flex-row mt-6" style="background-image: url('../src/lib/image/pemandangan.jpg');">
+            <!-- Bagian Kiri (Renungan) -->
+            <div class="flex items-center justify-center bg-black bg-opacity-50 text-white w-1/2 rounded-lg">
+                <h1 class="text-2xl font-bold">Renungan</h1>
+            </div>
+            <!-- Bagian Kanan (Deskripsi) dengan Background Putih Transparan -->
+            <div class="bg-white bg-opacity-50 p-4 w-1/2 rounded-lg">
+                <h1>Suatu kali saya mendapatkan panggilan masuk dari sebuah nomor yang tidak dikenal. Ia memperkenalkan diri sebagai 
+                    seorang teman di luar kota yang baru saja berganti nomor. Ia menyebut nama diri dengan lengkap, beserta alamat rumahnya. 
+                    Dilihat dari media sosialnya pun, foto profilnya sesuai. Meski demikian, kami mencurigainya karena suara dan gaya 
+                    bicaranya sangat berbeda. Dugaan kami terbukti setelah kami menelepon teman kami yang nama dan fotonya dibajak.</h1>
+            </div>
+        </div>
+
+        <!-- Highlight acara -->
+        <div class="relative bg-cover bg-center w-full h-64 rounded-lg shadow-lg flex flex-row mt-6" style="background-image: url('../src/lib/image/pemandangan.jpg');">
+            <!-- Bagian Kiri (Renungan) -->
+            <div class="flex items-center justify-center bg-black bg-opacity-50 text-white w-1/2 rounded-lg">
+                <h1 class="text-2xl font-bold">Renungan</h1>
+            </div>
+            <!-- Bagian Kanan (Deskripsi) dengan Background Putih Transparan -->
+            
+        </div>
+        
+    </div>
+</div>
--- Website_user/src/routes/login/+page.svelte
+++ Website_user/src/routes/login/+page.svelte
@@ -1,6 +1,6 @@
 <div class="h-screen w-screen flex flex-col" style="background-image: url('../src/lib/image/salib.jpg'); background-size: cover;">
     <div class="flex justify-center items-center flex-1">
-        <img src="../src/lib/image/logo_white.png" alt="">
+        <img src="../src/lib/image/logo_white.png" alt="logo">
     </div>
     <div class="bg-base flex flex-col flex-1 opacity-50">
         <div class="flex justify-center">
@@ -10,16 +10,16 @@
             <p class="text-white w-1/5">Enter your Username to Login for this app</p>
         </div>
         <div class="flex justify-center mt-4">
-            <input type="text" placeholder="Username" class="w-1/5 placeholder-black">
+            <input id="" type="text" placeholder="Username" class="w-1/5 placeholder-black">
         </div>
         <div class="flex justify-center mt-4">
-            <input type="password" placeholder="Password" class="w-1/5 placeholder-black">
+            <input id="" type="password" placeholder="Password" class="w-1/5 placeholder-black">
         </div>
         <div class="flex justify-center mt-6">
-            <button class="bg-[#F9C067] text-white px-6 py-2 rounded hover:bg-[#F8B048] font-bold">Login</button>
+            <button class="bg-[#F9C067] px-8 py-2 rounded hover:bg-[#F8B048] font-bold">Login</button>
         </div>
         <div class="flex justify-center mt-2">
-            <a href="#" class="text-[#F9C067] underline">Forgot Password</a>
+            <a href="forgot_password" class="text-[#F9C067] underline">Forgot Password</a>
         </div>
     </div>
 </div>
