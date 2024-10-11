0f792d92a285c7cd4e3379dda2d77394fcc576b2
jjehian <jjehian000@gmail.com>
2024-10-11 16:36:48 +0700

    navbar dasboard

--- Website_manajement/src/routes/+page.svelte
+++ Website_manajement/src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-screen w-screen flex">
+    <div class="basis-1/2 bg-base">
+        <div class="flex justify-center items-center h-full">
+            <h1 class="text-7xl font-bold text-center text-white">Log in</h1>
+        </div>
+    </div>
+    
+    <div class="basis-1/2 items-center justify-center h-full flex">
+        <div class="flex flex-col items-center w-full px-10">
+            <h1 class="text-2xl text-center text-base mb-4">Enter your Username to Login for this app</h1>
+            <input type="text" class="border border-black rounded-md p-1 w-full max-w-lg" placeholder="Username" />
+            <input type="password" class="mt-2 border border-black rounded-md p-1 w-full max-w-lg" placeholder="Password" />
+            <a href="/admin/dashboard" class="mt-5 bg-base text-1xl text-black text-center p-2 rounded-full w-full hover:bg-slate-300 hover:border-[1px] hover:border-black ease-in duration-400 max-w-lg">Login</a> 
+            <!-- <a href="" class="mt-auto text-amber-500 underline">Forgot Password?</a> -->
+        </div>
+    </div>
+</div>
---
+++ Website_manajement/src/routes/admin/dashboard/+page.svelte
@@ -0,0 +1,2 @@
+<div class="bg-base h-screen w-screen">
+</div>
