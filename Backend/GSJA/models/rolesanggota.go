71ba4968f4713b33f8aaf827f39ddf80e3aae816
Christian Adi <c14220002@john.petra.ac.id>
2024-12-01 17:40:45 +0700

    get roles profile

--- Backend/GSJA/models/rolesanggota.go
+++ Backend/GSJA/models/rolesanggota.go
@@ -10,6 +10,6 @@ type RolesAnggota struct {
 	IdRoles    int        `json:"id_roles" db:"id_roles"`
 	CreatedAt time.Time  `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time  `json:"updated_at" db:"updated_at"`
-	DeletedAt time.Time  `json:"deleted_at" db:"deleted_at"`
+	DeletedAt *time.Time  `json:"deleted_at" db:"deleted_at"`
 
 }
